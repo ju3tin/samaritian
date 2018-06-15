2494cefa99ca96562c8e24903f7e145cd7867729
Justin Grierson <ju3tin@hotmail.co.uk>
2018-06-15 11:43:27 +0100

    update database 15/06/2018

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
    define('DB_USERNAME', 'ba8b59cfea5cda');
    define('DB_PASSWORD', 'a701c56b');
    define('DB_DATABASE', 'ibmx_fa8d6555e096b37');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $db = mysql_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
