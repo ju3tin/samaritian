ca56eba8a9195e0694ef4e4459b18c35233082c4
Justin Grierson <ju3tin@hotmail.co.uk>
2018-06-15 10:29:29 +0100

    update database 15/06/2018

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'eu-cdbr-sl-lhr-01.cleardb.net');
+   define('DB_SERVER', 'eu-cdbr-sl-lhr-01.cleardb.net:3306');
    define('DB_USERNAME', 'ba8b59cfea5cda');
    define('DB_PASSWORD', 'a701c56b');
    define('DB_DATABASE', 'ibmx_fa8d6555e096b37');
