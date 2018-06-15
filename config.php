36d000651a0af59c541b87dfe81387e15535d17d
Justin Grierson <ju3tin@hotmail.co.uk>
2018-06-15 10:27:18 +0100

    update database 15/06/2018

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootpassword');
-   define('DB_DATABASE', 'database');
+   define('DB_SERVER', 'eu-cdbr-sl-lhr-01.cleardb.net');
+   define('DB_USERNAME', 'ba8b59cfea5cda');
+   define('DB_PASSWORD', 'a701c56b');
+   define('DB_DATABASE', 'ibmx_fa8d6555e096b37');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
